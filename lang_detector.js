1494d2c689246a9b7d98c704daf83a0d5c902c56
absucc <57920717+absucc@users.noreply.github.com>
2020-12-17 18:02:58 -0300

    Create lang_detector.js

---
+++ lang_detector.js
@@ -0,0 +1,142 @@
+var theLang = navigator.language || navigator.userLanguage;
+
+var isAfrikaans;
+var isArabic;
+var isAzeri;
+var isBelarusian;
+var isBulgarian;
+var isCatalan;
+var isCzech;
+var isWelsh;
+var isSpanish;
+var isEnglish;
+
+if (theLang=="af") {
+    /* Afrikaans */
+    isAfrikaans = true;
+} else if (theLang=="af-ZA") {
+    isAfrikaans = true;
+} else if (theLang=="ar") {
+    /* Arabic */
+    isArabic = true;
+} else if (theLang=="ar-AE") {
+    isArabic = true;
+} else if (theLang=="ar-BH") {
+    isArabic = true;
+} else if (theLang=="ar-DZ") {
+    isArabic = true;
+} else if (theLang=="ar-EG") {
+    isArabic = true;
+} else if (theLang=="ar-IQ") {
+    isArabic = true;
+} else if (theLang=="ar-JO") {
+    isArabic = true;
+} else if (theLang=="ar-KW") {
+    isArabic = true;
+} else if (theLang=="ar-LB") {
+    isArabic = true;
+} else if (theLang=="ar-LY") {
+    isArabic = true;
+} else if (theLang=="ar-MA") {
+    isArabic = true;
+} else if (theLang=="ar-OM") {
+    isArabic = true;
+} else if (theLang=="ar-QA") {
+    isArabic = true;
+} else if (theLang=="ar-SA") {
+    isArabic = true;
+} else if (theLang=="ar-SY") {
+    isArabic = true;
+} else if (theLang=="ar-TN") {
+    isArabic = true;
+} else if (theLang=="ar-YE") {
+    isArabic = true;
+} else if (theLang=="az") {
+    /* Azeri */
+    isAzeri = true;
+} else if (theLang=="az-AZ") {
+    isAzeri = true;
+} else if (theLang=="be") {
+    /* Belarusian */
+    isBelarusian = true;
+} else if (theLang=="be-BY") {
+    isBelarusian = true;
+} else if (theLang=="bg") {
+    /* Bulgarian */
+    isBulgarian = true;
+} else if (theLang=="bg-BG") {
+    isBulgarian = true;
+} else if (theLang=="bg-BA") {
+    isBulgarian = true;
+} else if (theLang=="ca") {
+    /* Catalan */
+    isCatalan = true;
+} else if (theLang=="ca-ES") {
+    isCatalan = true;
+} else if (theLang=="cs") {
+    /* Czech */
+    isCzech = true;
+} else if (theLang=="cs-CZ") {
+    isCzech = true;
+} else if (theLang=="cy") {
+    /* Welsh */
+    isWelsh = true;
+} else if (theLang=="cy-GB") {
+    isWelsh = true;
+} else if (theLang=="da") {
+    isDanish = true;
+} else if (theLang=="da-DK") {
+    isDanish = true;
+} else if (theLang=="de")
+} else if (theLang=="es") {
+    /* Spanish */
+    isSpanish = true;
+} else if (theLang=="es-419") {
+    isSpanish = true;
+} else if (theLang=="es-AR") {
+    isSpanish = true;
+} else if (theLang=="es-BO") {
+    isSpanish = true;
+} else if (theLang=="es-CL") {
+    isSpanish = true;
+} else if (theLang=="es-CO") {
+    isSpanish = true;
+} else if (theLang=="es-CR") {
+    isSpanish = true;
+} else if (theLang=="es-DO") {
+    isSpanish = true;
+} else if (theLang=="es-EC") {
+    isSpanish = true;
+} else if (theLang=="es-ES") {
+    isSpanish = true;
+} else if (theLang=="es-GT") {
+    isSpanish = true;
+} else if (theLang=="es-HN") {
+    isSpanish = true;
+} else if (theLang=="es-MX") {
+    isSpanish = true;
+} else if (theLang=="es-NI") {
+    isSpanish = true;
+} else if (theLang=="es-PA") {
+    isSpanish = true;
+} else if (theLang=="es-PE") {
+    isSpanish = true;
+} else if (theLang=="es-PR") {
+    isSpanish = true;
+} else if (theLang=="es-PY") {
+    isSpanish = true;
+} else if (theLang=="es-SV") {
+    isSpanish = true;
+} else if (theLang=="es-UY") {
+    isSpanish = true;
+} else if (theLang=="es-VE") {
+    isSpanish = true;
+} else {
+    /* ENGLISH */
+    document.write("<meta http-equiv=\"Refresh\" content=\"0; url='en.html'\" />");
+}
+function langDetect() {
+  if (isSpanish == true) {
+    return spanish
+  } else if (is)
+}
